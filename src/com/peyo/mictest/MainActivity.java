75ab632a1e5e9534defc87b31ee3b188be5fc531
Peter Yoon <peyo.hd@gmail.com>
2017-02-17 07:29:42 +0900

    test sine-wave

--- src/com/peyo/mictest/MainActivity.java
+++ src/com/peyo/mictest/MainActivity.java
@@ -10,88 +10,77 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MicTest";
+    private EditText mNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
-        requestPermissions(new String[] {Manifest.permission.RECORD_AUDIO}, 0);
+        mNumber = (EditText)findViewById(R.id.freq);
+        mPlaying = false;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-        mEchoing = false;
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mEchoing = false;
-    }
-
-    private static final int SAMPLE_RATE = 8000;
-    private static final int SAMPLE_ENCODING = AudioFormat.ENCODING_PCM_8BIT;
-
-    AudioRecord mRecorder;
-    AudioTrack mTrack;
-    byte mBuffer[];
-    boolean mEchoing;
+    boolean mPlaying;
+    float mFreq;
 
     public void onEchoClicked(View v) {
-        Log.i(TAG, "onEchoClicked()");
+        Log.i(TAG, "onPlayClicked()");
 
-        if (mEchoing) return;
+        if (mPlaying) return;
 
-        mEchoing = true;
+        mPlaying = true;
 
-        int bufSize = getBufferSize();
-
-        mBuffer = new byte[bufSize];
-
-        mRecorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE,
-                AudioFormat.CHANNEL_IN_MONO, SAMPLE_ENCODING, mBuffer.length);
-
-        mTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
-                SAMPLE_ENCODING, mBuffer.length, AudioTrack.MODE_STREAM);
+        String num = mNumber.getText().toString();
+        mFreq = Integer.parseInt(num);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                mTrack.play();
-                mRecorder.startRecording();
-                while (mEchoing) {
-                    mRecorder.read(mBuffer, 0, mBuffer.length);
-                    mTrack.write(mBuffer, 0, mBuffer.length);
+                short[] buffer = new short[1024];
+                AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO, SAMPLE_ENCODING, getBufferSize(), AudioTrack.MODE_STREAM);
+                float increment = (float)((2.0 * Math.PI) * mFreq / 48000.0); // angular increment for each sample
+                float angle = 0;
+                float samples[] = new float[1024];
+
+                track.play();
+
+                while (mPlaying) {
+                    for (int i = 0; i < samples.length; i++) {
+                        samples[i] = (float) Math.sin(angle);   //the part that makes this a sine wave....
+                        buffer[i] = (short) (samples[i] * Short.MAX_VALUE / 2);
+                        angle += increment;
+                    }
+                    track.write( buffer, 0, samples.length );  //write to the audio buffer.... and start all over again!
+
                 }
-                mRecorder.stop();
-                mTrack.stop();
             }
         }).start();
     }
 
+    private static final int SAMPLE_RATE = 48000;
+    private static final int SAMPLE_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private int getBufferSize() {
+                int outSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, SAMPLE_ENCODING);
+                outSize = Integer.highestOneBit(outSize) * 2;
+
+                return outSize;
+            }
+
 
     public void onStopClicked(View v) {
         Log.i(TAG, "onStopClicked()");
-        mEchoing = false;
+        mPlaying = false;
     }
-
-    private int getBufferSize() {
-        int outSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, SAMPLE_ENCODING);
-        outSize = Integer.highestOneBit(outSize) * 2;
-
-        int inSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, SAMPLE_ENCODING);
-        inSize = Integer.highestOneBit(inSize) * 2;
-
-        int bufSize = (outSize > inSize) ? outSize : inSize;
-
-        Log.i(TAG, "bufferSize " + bufSize);
-        return bufSize;
-    }
-
 
 }
